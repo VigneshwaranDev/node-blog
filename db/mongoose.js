f787ba6e594a11fd2f9ac7bb50dca6a9c599e14f
Vicky <vigneshwaranr28@gmail.com>
2020-08-10 11:26:16 +0530

    Added .env file for the cloud mongoDB server

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const url = "mongodb://localhost:27017/blog";
+const url = process.env.URL;
 
 mongoose.connect(url, {
   useNewUrlParser: true,
